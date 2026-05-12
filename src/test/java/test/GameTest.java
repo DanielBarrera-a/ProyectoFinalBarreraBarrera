1df58a25260e6d9119aa635be440f36ff45177eb
DanielBarrera-a <daniel.barrera-a@mail.escuelaing.edu.co>
2026-05-11 20:48:23 -0500

    Corregir

--- src/test/java/test/GameTest.java
+++ src/test/java/test/GameTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameTest {
 
     // HELPER
+    //Corregir test
 
     private CellType[][] createEmptyBoard() {
         CellType[][] board = new CellType[5][5];
